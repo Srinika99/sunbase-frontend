d912a70f47e53347430a229c7da27e0da656b2d4
Srinika Semwal <srinika24@icloud.com>
2023-08-02 21:32:13 +0530

    chore: added the list api

---
+++ src/Apis/list.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export async function getCustomerList(accessToken) {
+  const apiUrl = "https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=get_customer_list";
+
+  try {
+    const response = await axios.get(apiUrl, {
+      headers: {
+        "Authorization": 'Bearer dGVzdEBzdW5iYXNlZGF0YS5jb206VGVzdEAxMjM=',
+        "Content-Type": "application/json",
+      },
+      mode: 'no-cors'
+    });
+
+    const data = response.data;
+
+    return data; 
+
+  } catch (error) {
+    console.error("Failed to fetch customer list:", error);
+    throw new Error("Failed to fetch customer list. Please try again later.");
+  }
+}
