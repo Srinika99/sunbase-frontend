e2495b8a7c3b695ee04ae42051c21e88e77925bc
Srinika Semwal <srinika24@icloud.com>
2023-08-02 22:23:57 +0530

    chore: handles navigation edge cases

--- src/App.js
+++ src/App.js
@@ -7,19 +7,34 @@ import NewUser from './pages/new-user';
 import UpdateUser from './pages/update';
 
 export default function App() {
+
+
+  const route = window.location.pathname;
+  const accessToken = localStorage.getItem("access_token")
+
+  // visiting invalid page
+  if (!["/login", "/list", "/new-user", "/update"].includes(route)) {
+    window.location.href = "/login"
+  }
+
+  // unauth access
+  if (!accessToken && route !== "/login") {
+    window.location.href = "/login"
+  }
+
   return (
     <div className="App">
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      {/* <Route path="/new" element={<New />} /> */}
-      <Route path="/list" element={<List />} />
-      <Route path="/new-user" element={<NewUser />} />
-      <Route path="/update" element={<UpdateUser />} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/new" element={<New />} /> */}
+        <Route path="/list" element={<List />} />
+        <Route path="/new-user" element={<NewUser />} />
+        <Route path="/update" element={<UpdateUser />} />
 
 
 
-    </Routes>
-  </div>
+      </Routes>
+    </div>
   )
 }
 
