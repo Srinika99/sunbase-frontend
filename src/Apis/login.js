16e372a960233ca11b3b8f0d6141c3f48471b409
Srinika Semwal <srinika24@icloud.com>
2023-08-02 21:25:39 +0530

    chore: added login page

---
+++ src/Apis/login.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export async function login(loginId, password) {
+  const apiUrl = "https://qa2.sunbasedata.com/sunbase/portal/api/assignment_auth.jsp";
+
+  try {
+    const response = await axios.post(apiUrl, {
+      login_id: loginId,
+      password: password,
+    }, {
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+        mode: 'no-cors'
+    });
+
+    const data = response.data;
+    window.localStorage.setItem("access_token", data.access_token);
+    window.location.href = '/list';
+
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        console.error(
+          "Login failed. Server responded with:",
+          error.response.data 
+        );
+      } else {
+        console.error("Login failed. No response received from the server.");
+      }
+    } else {
+      console.error("Login failed. Error:", error);
+    }
+    throw new Error("Login failed");
+  }
+}
+
+
---
+++ src/pages/login.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { login } from "../Apis/login";
+
+export default function Login() {
+  const [loginId, setLoginId] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+    setError("");
+
+    try {
+      const response = await login(loginId, password);
+      console.log("Login response:", response);
+    } catch (error) {
+      console.error("Login error:", error);
+      setError("Failed to sign in. Please check your credentials.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="loginId"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Login ID
+            </label>
+            <div className="mt-2">
+              <input
+                id="loginId"
+                name="loginId"
+                type="text"
+                autoComplete="loginId"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={loginId}
+                onChange={(e) => setLoginId(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {isLoading ? "Signing in..." : "Sign in"}
+            </button>
+          </div>
+        </form>
+
+        {error && (
+          <p className="mt-3 text-center text-sm text-red-500">{error}</p>
+        )}
+
+      </div>
+    </div>
+  );
+}
