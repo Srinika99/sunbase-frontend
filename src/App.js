c82fe4a22ee2ac9ced2e5294f70cf84c32574212
Srinika Semwal <srinika24@icloud.com>
2023-08-02 21:22:39 +0530

    chore: setting up the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/login';
+import { Routes, Route } from 'react-router-dom';
+// import New from './pages/new';
+import List from './pages/list';
+import NewUser from './pages/new-user';
+import UpdateUser from './pages/update';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App">
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      {/* <Route path="/new" element={<New />} /> */}
+      <Route path="/list" element={<List />} />
+      <Route path="/new-user" element={<NewUser />} />
+      <Route path="/update" element={<UpdateUser />} />
+
+
+
+    </Routes>
+  </div>
   )
 }
 
